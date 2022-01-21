e3cf02e2cb0bcb4ae87dc5dc2ba6717f87365718
Matheus Gomes <mgrspeed@gmail.com>
2022-01-21 17:21:08 -0300

    @Rorschach616 [Task: Add menu and footer]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentMenuRodapeComponent } from './component-menu-rodape/component-menu-rodape.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentMenuRodapeComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component-menu-rodape/component-menu-rodape.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-menu-rodape',
+  templateUrl: './component-menu-rodape.component.html',
+  styleUrls: ['./component-menu-rodape.component.css']
+})
+export class ComponentMenuRodapeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
