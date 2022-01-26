295b480921989e16b014df0b49444aeba84d3360
Bianca Barros <biancabarrosufrj@gmail.com>
2022-01-26 11:45:36 -0300

    @barrosbianca [Task: Criação das models: User, Login, Postagem e Grupo]

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,14 @@
+export class UserLogin {
+    public idUsuario: number
+	public nomeUsuario: string
+	public email: string
+	public senha: string
+	public bio: string
+	public urlAvatar: string
+	public nivel: number
+	public xp: number
+	public token: string
+	public tokenBasic: string
+	public foto: string
+	public tipo: string
+}
